f7774264ff2cefc797d57b6283672e523517dae8
Tony Hu <Tony.Hu@amd.com>
2023-06-01 11:25:06 -0400

    Add autocmpl.lua

---
+++ dot_config/nvim/lua/autocmpl.lua
@@ -0,0 +1,55 @@
+local cmp = require('cmp')
+
+require('cmp_luasnip_choice').setup {
+    auto_open = true
+}
+
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert {
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                local entry = cmp.get_selected_entry()
+                if not entry then
+                    cmp.select_next_item()
+                else
+                  cmp.confirm()
+            end
+            else
+                fallback()
+            end
+        end, {"i","s","c",}
+        ),
+    },
+   sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip_choice' },
+    }
+}
+
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    },
+})
+
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        {
+            { name = 'path' },
+        },
+        {
+            { name = 'cmdline'},
+        },
+    },
+})
