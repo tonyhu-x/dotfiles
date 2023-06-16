d46c87d9fc0bbc8fc289bbe5f58bad38ca4e1cd3
Tony Hu <Tony.Hu@amd.com>
2023-06-16 09:53:50 -0400

    Add more VSCode shortcuts in Neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -24,6 +24,7 @@ init_vscode = function()
     keymap('', '<leader>e', "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>", opts)
     keymap('', '<leader>E', "<Cmd>call VSCodeNotify('workbench.files.action.showActiveFileInExplorer')<CR>", opts)
     keymap('', '<leader>p', "<Cmd>call VSCodeNotify('workbench.action.showCommands')<CR>", opts)
+    keymap('', '<leader>f', "<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>", opts)
 
     keymap('i', '<C-c>', '<Esc>', opts)
 
@@ -33,6 +34,7 @@ init_vscode = function()
     keymap({'n', 'v'}, 'gr', "<Cmd>call VSCodeNotify('editor.action.referenceSearch.trigger')<CR>", opts)
     keymap({'n', 'v'}, 'gh', "<Cmd>call VSCodeNotify('editor.action.showHover')<CR>", opts)
     keymap({'n', 'v'}, 'go', "<Cmd>call VSCodeNotify('workbench.action.gotoSymbol')<CR>", opts)
+    keymap({'n', 'v'}, 'gf', "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>", opts)
 
     keymap('n', '<leader>r', "<Cmd>call VSCodeNotify('editor.action.rename')<CR>", opts)
 
