64e4ccd3f3a6b58f03a132537b0f9bade71d67ba
Tony Juntao Hu <54758881+tonyhu-x@users.noreply.github.com>
2024-06-16 21:34:04 -0400

    Add support for Typst in Neovim

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -4,6 +4,15 @@ return {
   opts = {
     servers = {
       rust_analyzer = {},
+      tinymist = {
+        --- todo: these configuration from lspconfig maybe broken
+        single_file_support = true,
+        root_dir = function()
+          return vim.fn.getcwd()
+        end,
+        --- See [Tinymist Server Configuration](https://github.com/Myriad-Dreamin/tinymist/blob/main/Configuration.md) for references.
+        settings = {},
+      },
     },
   },
 }
