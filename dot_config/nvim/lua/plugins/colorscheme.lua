4642bceadd6526a7c2586542e9456d9289448c59
Tony Juntao Hu <54758881+tonyhu-x@users.noreply.github.com>
2024-07-18 11:09:57 +0800

    Disable catppuccin lazy loading

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,10 @@ return {
     "sainnhe/gruvbox-material",
   },
   {
+    "catppuccin/nvim",
+    lazy = false,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox-material",
